4cee54ffb5ea02f63218c1c1d030d5dafe821ac5
ayda <aydashamma4@gmail.com>
2025-02-10 04:42:59 -0800

    completed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Header } from "./components/header";
 import { MainSec } from "./components/main-sec";
 import { Nav } from "./components/nav";
 
-// import "./App.css"
+
 
 function App() {
  return (
