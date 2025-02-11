a802ca806dd30c9da2b4c054f4699ad91487d979
ayda <aydashamma4@gmail.com>
2025-02-11 09:25:41 -0800

    created servicesBox.jsx and servicesBox.module.css components

--- src/components/main-sec.jsx
+++ src/components/main-sec.jsx
@@ -2,17 +2,29 @@ import { Button } from "./button";
 import { Card } from "./card";
 
 import styles from "./mainsec.module.css"
+import { ServicesBox } from "./servicesBox";
 
 export function MainSec() {
     return (
     
          <section>
            <div className={styles.featuressection}>
-           <Button/>
-            <h2>Our Special Features</h2>
-           <p className={styles.par}>Our kinder garden school provides a nurturing and stimulating environment, fostering a love for learning that lasts a lifetime. Join us as we embark on an exciting educational journey together!</p>
-            <Card/>
+              <Button/>
+              <h2>Our Special Features</h2>
+              <p className={styles.par}>Our kinder garden school provides a nurturing and stimulating environment, fostering a love for learning that lasts a lifetime. Join us as we embark on an exciting educational journey together!</p>
+              <Card/>
            </div>
+
+           <div>
+              <Button/>
+              <h2>What Students Learn</h2>
+              <p>
+                At Little Learners Academy, we strive to cultivate a love for learning and equip children with essential skills for their future success. Our academic programs cover a wide range of subjects, allowing students to develop a strong foundation and discover their interests. Some key areas of learning include
+              </p>
+           
+              <ServicesBox/>
+           </div>
+          
         </section>
     
     )
---
+++ src/components/servicesBox.jsx
@@ -0,0 +1,13 @@
+
+export function ServicesBox() {
+    return (
+        <div> 
+
+              <img src="" alt="" />
+              <h4>Language Arts</h4>
+              <p>Reading, writing, storytelling, and communication skills.</p>
+        
+          
+        </div>
+    )
+}
